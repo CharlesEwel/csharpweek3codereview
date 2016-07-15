3d8740faf10570873f6a85b755e401183298655b
Charles Ewel <Charles.Ewel@gmail.com>
2016-07-15 11:24:03 -0700

    add views and home module that allow user to add stylists and then assign them clients

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+using HairSalon.Objects;
+using System;
+
+namespace HairSalon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"]=_=> View["index.cshtml"];
+
+      DELETE["/"]
+
+      Get["/stylists"]=_=>{
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["stylists.cshtml", allStylists];
+      };
+
+      Get["/stylist/new"]=_=>View["stylist_new.cshtml"];
+
+      Post["/stylist/new/success"]=_=>{
+        Stylist newStylist = new Stylist(
+                                          Request.Form["stylist-name"],
+                                          Request.Form["price"]
+                                        );
+        newStylist.Save();
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["stylists.cshtml", allStylists];
+      };
+
+      Get["/clients"]=_=>{
+        List<Client> allClients = Client.GetAll();
+        return View["clients.cshtml", allClients];
+      };
+
+      Get["/client/new"]=_=>{
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["client_new.cshtml", allStylists];
+      };
+
+      Post["/client/new/success"]=_=>{
+        Client newClient = new Client(
+                                          Request.Form["client-name"],
+                                          Request.Form["hair-color"],
+                                          Request.Form["stylist-id"]
+                                        );
+        newClient.Save();
+        List<Client> allClients = Client.GetAll();
+        return View["clients.cshtml", allClients];
+      };
+    }
+  }
+}
